60adefbd0841ea5ffbe971859a2f0349ee9ee991
Dave Winer <dave.winer@gmail.com>
2017-08-04 09:04:46 -0400

    v0.4.0

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# feedParserDemo
-A demo of Dan MacTough's feedparser package for Node.
+## feedParserDemo
+
+A demo of Dan MacTough's feedparser package.
+
+### The story
+
+I use Dan's feedparser package in River5, which is my latest river-of-news feed reader. So all my news flows through this package.
+
+It was a little tricky getting started because the package is designed for maximum efficiency at the expense of a bit of ease-of-use especially for a Node newbie. So I pulled out a bit of code from River5 and simplified it so it makes a good demo of feedparser. 
+
+If you're new to feedparser, this might help you get started a bit faster with less head-scratching. :-)
+
+### How to
+
+1. Download the project. 
+
+2. Open the folder in your Terminal app. 
+
+3. npm install
+
+4. node demo.js
+
+You should see a list of item titles from the feed. 
+
---
+++ demo.js
@@ -0,0 +1,76 @@
+var myProductName = "feedParserDemo"; myVersion = "0.4.0";
+
+/*  The MIT License (MIT)
+	Copyright (c) 2014-2017 Dave Winer
+	
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+	
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+	*/
+
+const request = require ("request");
+const FeedParser = require ("feedparser");
+
+const urlTestFeed = "http://www.guardian.co.uk/world/usa/rss";
+
+function getFeed (urlfeed, callback) {
+	var req = request (urlfeed);
+	var feedparser = new FeedParser ();
+	var feedItems = new Array ();
+	req.on ("response", function (res) {
+		var stream = this;
+		if (res.statusCode == 200) {
+			stream.pipe (feedparser);
+			}
+		});
+	req.on ("error", function (res) {
+		console.log ("getFeed: Error reading feed.");
+		});
+	feedparser.on ("readable", function () {
+		try {
+			var item = this.read (), flnew;
+			if (item !== null) { //2/9/17 by DW
+				feedItems.push (item);
+				}
+			}
+		catch (err) {
+			console.log ("getFeed: err.message == " + err.message);
+			}
+		});
+	feedparser.on ("end", function () {
+		callback (feedItems);
+		});
+	feedparser.on ("error", function () {
+		console.log ("getFeed: Error reading feed.");
+		callback (undefined);
+		});
+	}
+
+getFeed (urlTestFeed, function (feedItems) {
+	console.log ("There are " + feedItems.length + " items in the feed.\n");
+	function pad (num) { 
+		var s = num.toString (), ctplaces = 3;
+		while (s.length < ctplaces) {
+			s = "0" + s;
+			}
+		return (s);
+		}
+	for (var i = 0; i < feedItems.length; i++) {
+		console.log ("Item #" + pad (i) + ": " + feedItems [i].title + ".\n");
+		}
+	});
+
